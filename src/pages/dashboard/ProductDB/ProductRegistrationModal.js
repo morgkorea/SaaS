f8c71d7da75ca5bb9b12fa28f4d507b66ac3ec97
hunhunseunghun <hunhunseunghun@gmail.com>
2023-06-14 16:01:52 +0900

    modal elemets build

--- src/pages/dashboard/ProductDB/ProductRegistrationModal.js
+++ src/pages/dashboard/ProductDB/ProductRegistrationModal.js
@@ -1,6 +1,6 @@
 // @flow
 import React, { useState } from 'react';
-import { Row, Col, Card, Button, Modal } from 'react-bootstrap';
+import { Row, Col, Card, Button, Modal, Alert } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import classNames from 'classnames';
 
@@ -13,8 +13,6 @@ import logodark from '../../../assets/images/logo-dark.png';
 const ProductRegistrationModal = ({ modal, setModal }) => {
     // const [modal, setModal] = useState(false);
     const [size, setSize] = useState('lg');
-    const [className, setClassName] = useState(null);
-    const [scroll, setScroll] = useState(null);
 
     /**
      * Show/hide the modal
@@ -23,48 +21,42 @@ const ProductRegistrationModal = ({ modal, setModal }) => {
         setModal(!modal);
     };
 
-    /**
-     * Opens large modal
-     */
-    const openModalWithSize = (size) => {
-        setSize(size);
-        setClassName(null);
-        setScroll(null);
-        toggle();
-    };
-
-    /**
-     * Opens modal with custom class
-     */
-    const openModalWithClass = (className) => {
-        setClassName(className);
-        setSize(null);
-        setScroll(null);
-        toggle();
-    };
-
-    /**
-     * Opens large modal
-     */
-    const openModalWithScroll = () => {
-        setScroll(true);
-        setSize(null);
-        setClassName(null);
-        toggle();
-    };
-
     return (
         <>
-            <Modal
-                show={modal}
-                onHide={toggle}
-                dialogClassName={className}
-                size={size}
-                scrollable={scroll}
-                centered={true}>
+            <Modal show={modal} onHide={toggle} size={size} centered={true}>
                 <Modal.Header className="border-bottom-0" onHide={toggle} closeButton></Modal.Header>
-                <Modal.Body style={{ height: '456px' }}>
-                    <h5 className="modal-title">상품 등록</h5>
+                <Modal.Body style={{ height: '456px', padding: '0px 60px' }}>
+                    <h5 className="modal-title mb-3">상품 등록</h5>
+                    <Alert variant="info" dismissible className="mb-3">
+                        <span className="fw-bold">패키지 상품 관련 안내</span> - 패키지는 각가의 단품상품 등록 후
+                        매출등록에서 금액조정을 해주셔야 해요.
+                    </Alert>
+                    <div className="container text-center">
+                        <Row>
+                            <Col>
+                                <input className="w-100"></input>
+                            </Col>
+                            <Col>
+                                <input className="w-100"></input>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col>
+                                <input className="w-100"></input>
+                            </Col>
+                            <Col>
+                                <input className="w-100"></input>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col>
+                                <input className="w-100"></input>
+                            </Col>
+                            <Col>
+                                <input className="w-100"></input>
+                            </Col>
+                        </Row>
+                    </div>
                 </Modal.Body>
                 <Modal.Footer className="d-flex justify-content-center border-top-0">
                     <Button variant="primary">등록</Button>
