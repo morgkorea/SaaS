1ae5ad8eafbce94a386b0caf560805af7bfb9485
SeungHun <hunhunseunghun@gmail.com>
2023-06-22 22:57:42 +0900

    sales registration doing

--- src/pages/dashboard/SalesDB/SalesRegistrationModal.js
+++ src/pages/dashboard/SalesDB/SalesRegistrationModal.js
@@ -10,15 +10,51 @@ import { firestoreDB } from '../../../firebase/firebase';
 import { firestoreProductsFieldSchema } from '../../../firebase/firestoreDbSchema';
 
 import salesRegistrationStep1 from '../../../assets/images/icons/png/salesRegistrationStep1.png';
+import salesRegistrationStep2 from '../../../assets/images/icons/png/salesRegistrationStep2.png';
+import salesRegistrationStep3 from '../../../assets/images/icons/png/salesRegistrationStep3.png';
+import salesRegistrationStep4 from '../../../assets/images/icons/png/salesRegistrationStep4.png';
 
 //loading spinner
 import Spinner from '../../../components/Spinner';
 
 const SalesRegistrationModal = ({ modal, setModal }) => {
+    const [registrationStep, setRegistrationStep] = useState(4);
     const [size, setSize] = useState('lg');
 
     const toggle = () => {
         setModal(!modal);
+    };
+
+    const switchRegistrationStepImg = (registrationStep) => {
+        switch (registrationStep) {
+            case 1:
+                return salesRegistrationStep1;
+            case 2:
+                return salesRegistrationStep2;
+            case 3:
+                return salesRegistrationStep3;
+            case 4:
+                return salesRegistrationStep4;
+
+            default:
+                return salesRegistrationStep1;
+        }
+    };
+
+    const swithRegistrationStepForm = (registrationStep) => {
+        switch (registrationStep) {
+            case 1:
+                return salesRegistrationStep1;
+            case 2:
+                return salesRegistrationStep2;
+            case 3:
+                return salesRegistrationStep3;
+            case 4:
+                return salesRegistrationStep4;
+
+            default:
+                return salesRegistrationStep1;
+        }
     };
 
     useEffect(() => {}, [modal]);
@@ -31,9 +67,9 @@ const SalesRegistrationModal = ({ modal, setModal }) => {
                     onHide={toggle}
                     style={{ margin: '12px 0px' }}
                     closeButton></Modal.Header>
-                <Modal.Body style={{ width: '100%', height: '615px', padding: '0px 60px' }}>
+                <Modal.Body style={{ width: '100%', height: '570px', padding: '0px 60px' }}>
                     <div style={{ width: '100%', paddingBottom: '32px' }}>
-                        <img src={salesRegistrationStep1} style={{ width: '100%' }} />
+                        <img src={switchRegistrationStepImg(registrationStep)} style={{ width: '100%' }} />
                     </div>
                     <h4 className="modal-title mb-3 ">회원 검색</h4>
 
@@ -59,10 +95,15 @@ const SalesRegistrationModal = ({ modal, setModal }) => {
                         <div className="mb-4">
                             <div style={{ marginBottom: '8px' }}>오디언스</div>
                             <div style={{ display: 'flex', border: '1px solid #DEE2E6' }}>
-                                <div className="font-24" style={{ marginLeft: '3px' }}>
-                                    <i className="mdi mdi-magnify" />
-                                </div>
-                                <select type="text" className="form-control" style={{ border: 'none', padding: 0 }}>
+                                {/* <select className="form-control" style={{ border: 'none', padding: 0 }}> */}
+                                <select
+                                    className="w-100 p-1"
+                                    style={{
+                                        height: '40px',
+                                        border: '1px solid #DEE2E6',
+                                        borderRadius: ' 2px',
+                                        cursor: 'pointer',
+                                    }}>
                                     <option value={true}>활성</option>
                                     <option value={false}>비활성</option>
                                 </select>
@@ -76,8 +117,10 @@ const SalesRegistrationModal = ({ modal, setModal }) => {
                         </Row>
                     </div>
                 </Modal.Body>
-                <Modal.Footer className="d-flex justify-content-center border-top-0 ">
-                    <Button variant="primary">다음</Button>
+                <Modal.Footer className="d-flex justify-content-center border-top-0" style={{ paddingBottom: '48px' }}>
+                    <Button variant="primary" disabled={true} style={{ width: '200px' }}>
+                        다음
+                    </Button>
                 </Modal.Footer>
             </Modal>
         </>
