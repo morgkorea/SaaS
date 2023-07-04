80eaad96c21e5cd7632e7f8c5970e461b8b6b601
hunhunseunghun <hunhunseunghun@gmail.com>
2023-07-04 15:17:20 +0900

    매출 데이터 시각화 테이블 작업 초기

---
+++ src/pages/dashboard/SalesDB/SalesTable.js
@@ -0,0 +1,55 @@
+// @flow
+import React from 'react';
+import { Row, Col, Card } from 'react-bootstrap';
+
+// components
+import PageTitle from '../../../components/PageTitle';
+import Table from '../../../components/Table';
+
+const SalesTable = ({ data, columns }): React$Element<any> => {
+    const sizePerPageList = [
+        {
+            text: '30',
+            value: 30,
+        },
+        {
+            text: '60',
+            value: 60,
+        },
+        {
+            text: '90',
+            value: 90,
+        },
+        // {
+        //     text: 'All',
+        //     value: data.length,
+        // },
+    ];
+    return (
+        <>
+            <PageTitle title={'매출 DB'} />
+
+            <Row>
+                <Col>
+                    <Card>
+                        <Card.Body>
+                            <Table
+                                columns={columns}
+                                data={data}
+                                pageSize={30}
+                                sizePerPageList={sizePerPageList}
+                                isSortable={true}
+                                pagination={true}
+                                isSearchable={true}
+                                tablePurpose={'salesDB'}
+                                productTablePlaceholder={'상품명 / 코드 검색'}
+                            />
+                        </Card.Body>
+                    </Card>
+                </Col>
+            </Row>
+        </>
+    );
+};
+
+export default SalesTable;
--- src/pages/dashboard/SalesDB/index.js
+++ src/pages/dashboard/SalesDB/index.js
@@ -3,8 +3,11 @@ import { Row, Col } from 'react-bootstrap';
 
 import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import Form from 'react-bootstrap/Form';
 
 import DefaultPagination from '../../../components/DefaultPagination.js';
+
+import SalesTable from './SalesTable.js';
 
 import SalesRegistrationModal from './SalesRegistrationModal.js';
 
@@ -25,29 +28,105 @@ const SalesDB = () => {
 
     // 수정하기
     const [editMode, setEditMode] = useState(false);
+
     const email = useSelector((state) => {
         return state.Auth?.user.email;
     });
-    const memberRef = collection(firestoreDB, 'Users', email, 'Members');
+    const firestoreSalesCollectionRef = collection(firestoreDB, 'Users', email, 'Sales');
 
-    const getMembers = async () => {
-        const data = await getDocs(memberRef);
-
-        setCurrentMembers(
-            data.docs.map((doc) => ({
-                id: doc.id,
-                ...doc.data(),
-            }))
-        );
+    const getSalesData = async () => {
+        const firestoreSalesData = await getDocs(firestoreSalesCollectionRef);
+        const firestoreSalesArray = [];
+        firestoreSalesData.forEach((sale) => {
+            const saleData = sale.data();
+            firestoreSalesArray.push(saleData);
+        });
+        console.log(firestoreSalesArray);
     };
 
     useEffect(() => {
-        getMembers();
+        getSalesData();
     }, []);
 
     const toggle = () => {
         setModal(!modal);
     };
+    // const tableColumns = [
+    //     {
+    //         id: '1', // 열 ID
+    //         accessor: 'paymentNumber', // 해당 열에 표시할 데이터 필드
+    //         Header: '결제번호', // 열 헤더 텍스트
+    //         sort: true,
+    //         // ... 추가적인 열 설정
+    //     },
+    //     {
+    //         id: '2', // 열 ID
+    //         accessor: 'paymentDate', // 해당 열에 표시할 데이터 필드
+    //         Header: '결제일', // 열 헤더 텍스트
+    //         sort: true,
+    //         // ... 추가적인 열 설정
+    //     },
+    //     {
+    //         id: '3', // 열 ID
+    //         accessor: 'type', // 해당 열에 표시할 데이터 필드
+    //         Header: '결제시간', // 열 헤더 텍스트
+    //         sort: true,
+
+    //     },
+    //     {
+    //         id: '4', // 열 ID
+    //         accessor: 'name', // 해당 열에 표시할 데이터 필드
+    //         Header: '이름', // 열 헤더 텍스트
+    //         sort: true,
+
+    //     },
+    //     {
+    //         id: '5',
+    //         accessor: 'phone',
+    //         Header: '전화번호',
+    //         sort: true,
+    //     },
+    //     {
+    //         id: '6',
+    //         accessor: 'memberNumber',
+    //         Header: '회원번호',
+    //         sort: true,
+    //     },
+    //     {
+    //         id: '7',
+    //         accessor: 'salesProducts',
+    //         Header: '유형',
+
+    //     },
+    //     {
+    //         id: '8',
+    //         accessor: 'salesProducts',
+    //         Header: '상품',
+    //         sort: true,
+    //     },
+    //     {
+    //         id: '9',
+    //         accessor: 'modifiedDate',
+    //         Header: '정상가',
+    //         sort: true,
+    //     },
+    // ];
+
+    //         Cell: ({ value, row }) => (
+    //             <Container className="d-flex p-0">
+    //                 {
+    //                     <Form className="pe-auto">
+    //                         <Form.Check
+    //                             type="switch"
+    //                             id={`custom-switch-${row.index}`}
+    //                             label={value ? '활성' : '비활성'}
+    //                             onChange={(event) => productsActivationHandler(event, row.index)}
+    //                             defaultChecked={value}
+    //                         />
+    //                     </Form>
+    //                 }
+    //             </Container>
+    //         ),
 
     return (
         <>
@@ -63,7 +142,7 @@ const SalesDB = () => {
                 theme="light"
             /> */}
             {modal && <SalesRegistrationModal modal={modal} setModal={setModal} />}
-
+            {/* <SalesTable data={productsData} columns={tableColumns} /> */}
             <div className="edit-btn-area avatar-md" style={{ zIndex: '100' }} onClick={toggle}>
                 <span className="avatar-title bg-primary text-white font-20 rounded-circle shadow-lg">
                     <i className="mdi mdi-plus" />
