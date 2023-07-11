b55399ccc92e1a7e9dace179552de1fd8cf7aae6
hunhunseunghun <hunhunseunghun@gmail.com>
2023-07-11 13:29:21 +0900

    상품등록 폼 유효성 검사 작업중

--- src/pages/dashboard/ProductDB/ProductRegistrationModal.js
+++ src/pages/dashboard/ProductDB/ProductRegistrationModal.js
@@ -9,6 +9,8 @@ import { doc, getDoc, setDoc, collection } from 'firebase/firestore';
 import { firestoreDB } from '../../../firebase/firebase';
 import { firestoreProductsFieldSchema } from '../../../firebase/firestoreDbSchema';
 
+import * as yup from 'yup';
+
 //loading spinner
 import Spinner from '../../../components/Spinner';
 
@@ -24,6 +26,14 @@ const ProductRegistrationModal = ({ modal, setModal, productsData }) => {
     const [regularPrice, setRegularPrice] = useState(0);
     const [activation, setActivation] = useState(true);
     const [userCode, setUserCode] = useState('');
+
+    const [validationError, setValidationError] = useState(false);
+
+    const schema = yup.object().shape({
+        productName: yup.string().required('상품명을 입력해주세요.'),
+        regularPrice: yup.number().required('상품 가격을 입력해주세요.').positive('상품 가격은 양수여야 합니다.'),
+    });
+
     /**
      * Show/hide the modal
      */
@@ -103,26 +113,49 @@ const ProductRegistrationModal = ({ modal, setModal, productsData }) => {
 
     const putFirestoreProductRegistration = async () => {
         setIsRegistering(true);
-        const productsCollectionRef = doc(collection(firestoreDB, 'Users', email, 'Products'));
-        const productCode = generateProductNumber(productType, expirationPeriod, productsData);
-
-        const productData = {
-            ...firestoreProductsFieldSchema,
-            productCode: productCode,
-            product: productName,
-            type: productType,
-            expirationPeriod: expirationPeriod,
-            expirationCount: expirationCount,
-            regularPrice: regularPrice,
-            activation: activation,
-            createdDate: new Date().toISOString().split('T')[0],
-            modifiedDate: new Date().toISOString().split('T')[0],
-        };
 
         try {
+            await schema.validate(
+                {
+                    productName,
+                    expirationCount,
+                    regularPrice,
+                },
+                { abortEarly: false }
+            );
+            // 유효성 검사를 통과한 경우 처리할 로직 작성
+
+            const productsCollectionRef = doc(collection(firestoreDB, 'Users', email, 'Products'));
+            const productCode = generateProductNumber(productType, expirationPeriod, productsData);
+
+            const productData = {
+                ...firestoreProductsFieldSchema,
+                productCode: productCode,
+                product: productName,
+                type: productType,
+                expirationPeriod: expirationPeriod,
+                expirationCount: expirationCount,
+                regularPrice: regularPrice,
+                activation: activation,
+                createdDate: new Date().toISOString().split('T')[0],
+                modifiedDate: new Date().toISOString().split('T')[0],
+            };
+            console.log('통과');
+
             await setDoc(productsCollectionRef, productData);
             setModal(!modal);
         } catch (error) {
+            // 유효성 검사에 실패한 경우 처리할 로직 작성
+            if (error instanceof yup.ValidationError) {
+                // 유효성 검사 오류 처리
+                const validationErrors = {};
+                error.inner.forEach((validationError) => {
+                    validationErrors[validationError.path] = validationError.message;
+                });
+
+                setValidationError(validationErrors);
+                console.log(validationError);
+            }
             console.log(error);
         }
 
@@ -143,11 +176,21 @@ const ProductRegistrationModal = ({ modal, setModal, productsData }) => {
         console.log(event.target.value);
     };
     const getExpirationCount = (event) => {
-        setExpirationCount(Number(event.target.value));
+        let count = event.target.value;
+        if (isNaN(Number(count)) || count < 0) {
+            count = 0;
+        }
+        setExpirationCount(Number(count));
+        console.log(expirationCount);
     };
 
     const getRegularPrice = (event) => {
-        setRegularPrice(Number(event.target.value));
+        let price = event.target.value;
+        if (isNaN(Number(price)) || price < 0) {
+            price = 0;
+        }
+        setRegularPrice(Number(price));
+        console.log(regularPrice);
     };
 
     useEffect(() => {
@@ -167,7 +210,7 @@ const ProductRegistrationModal = ({ modal, setModal, productsData }) => {
         <>
             <Modal show={modal} onHide={toggle} size={size} centered={true}>
                 <Modal.Header className="border-bottom-0" onHide={toggle} closeButton></Modal.Header>
-                <Modal.Body style={{ height: '456px', padding: '0px 60px' }}>
+                <Modal.Body style={{ height: '430px', padding: '0px 60px' }}>
                     <h4 className="modal-title mb-3 ">상품 등록</h4>
                     <Alert variant="info" className="mb-3" style={{ color: '#1E5B6D' }}>
                         <span className="fw-bold">패키지 상품 관련 안내</span> - 패키지는 각각의 단품상품 등록 후
@@ -192,6 +235,7 @@ const ProductRegistrationModal = ({ modal, setModal, productsData }) => {
                                         border: '1px solid #DEE2E6',
                                         borderRadius: ' 2px',
                                     }}></input>
+                                {validationError.productName}
                             </Col>
                             <Col className="">
                                 <div className="mb-1">
@@ -266,14 +310,16 @@ const ProductRegistrationModal = ({ modal, setModal, productsData }) => {
                                     onChange={(e) => {
                                         getExpirationCount(e);
                                     }}
-                                    type="number"
+                                    type="text"
                                     defaultValue="0"
                                     className="w-100 p-1"
                                     style={{
                                         height: '40px',
                                         border: '1px solid #DEE2E6',
                                         borderRadius: ' 2px',
-                                    }}></input>
+                                    }}
+                                    value={expirationCount}
+                                    disabled={productType === 'locker' || productType === 'etc'}></input>
                             </Col>
                         </Row>
                         <Row className="mb-4">
@@ -283,19 +329,23 @@ const ProductRegistrationModal = ({ modal, setModal, productsData }) => {
                                     <span>상품 가격</span>
                                 </div>
                                 <input
-                                    type="number"
-                                    defaultValue={0}
+                                    type="text"
+                                    // defaultValue={0}
                                     className="w-100 p-1"
                                     onChange={(e) => {
                                         getRegularPrice(e);
                                     }}
+                                    value={regularPrice}
                                     style={{
                                         height: '40px',
                                         border: '1px solid #DEE2E6',
                                         borderRadius: ' 2px',
                                     }}></input>
+
                                 <span style={{ position: 'absolute', right: '20px', bottom: '10px' }}>원</span>
+                                <div> {validationError.regularPrice}</div>
                             </Col>
+
                             <Col>
                                 <div className="mb-1">
                                     <span>판매등록</span>
@@ -309,12 +359,19 @@ const ProductRegistrationModal = ({ modal, setModal, productsData }) => {
                                         style={{
                                             width: '150px',
                                             height: '40px',
-                                            border: `1px solid ${activation ? '#727CF5' : '#DEE2E6'}`,
-                                            color: activation ? '#727CF5' : '#DEE2E6',
+                                            border: `1px solid ${
+                                                activation ? (productType === 'etc' ? '#DEE2E6' : '#727CF5') : '#DEE2E6'
+                                            }`,
+                                            color: activation
+                                                ? productType === 'etc'
+                                                    ? '#DEE2E6'
+                                                    : '#727CF5'
+                                                : '#DEE2E6',
 
                                             borderRadius: ' 2px',
                                             backgroundColor: '#FFFFFF',
-                                        }}>
+                                        }}
+                                        disabled={productType === 'etc'}>
                                         판매등록
                                     </button>
                                     <button
@@ -324,11 +381,22 @@ const ProductRegistrationModal = ({ modal, setModal, productsData }) => {
                                         style={{
                                             width: '150px',
                                             height: '40px',
-                                            border: `1px solid ${!activation ? '#727CF5' : '#DEE2E6'}`,
-                                            color: !activation ? '#727CF5' : '#DEE2E6',
+                                            border: `1px solid ${
+                                                !activation
+                                                    ? productType === 'etc'
+                                                        ? '#DEE2E6'
+                                                        : '#727CF5'
+                                                    : '#DEE2E6'
+                                            }`,
+                                            color: !activation
+                                                ? productType === 'etc'
+                                                    ? '#DEE2E6'
+                                                    : '#727CF5'
+                                                : '#DEE2E6',
                                             borderRadius: ' 2px',
                                             backgroundColor: '#FFFFFF',
-                                        }}>
+                                        }}
+                                        disabled={productType === 'etc'}>
                                         보류하기
                                     </button>
                                 </div>
@@ -336,8 +404,11 @@ const ProductRegistrationModal = ({ modal, setModal, productsData }) => {
                         </Row>
                     </div>
                 </Modal.Body>
-                <Modal.Footer className="d-flex justify-content-center border-top-0">
-                    <Button onClick={putFirestoreProductRegistration} variant="primary">
+                <Modal.Footer className="d-flex justify-content-center border-top-0 ">
+                    <Button
+                        onClick={putFirestoreProductRegistration}
+                        variant="primary"
+                        style={{ marginBottom: '36px' }}>
                         {isRegistering ? (
                             <Spinner
                                 className="me-1"
