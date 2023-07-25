1dcecb8512e031963dd5c23516e5aa2482e794a4
simhaeun <7351_@daum.net>
2023-07-25 12:25:44 +0900

    회원 대시보드 table sort 기능 추가

---
+++ src/pages/dashboard/Members/SortableTable.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { useTable, useSortBy } from 'react-table';
+import { Table } from 'react-bootstrap';
+
+const SortableTable = ({ columns, data }) => {
+  const { getTableBodyProps, headerGroups, rows, prepareRow } = useTable(
+    {
+      columns: React.useMemo(() => columns, [columns]),
+      data: React.useMemo(() => data, [data]),
+    },
+    useSortBy
+  );
+
+  return (
+    <Table responsive className="table table-sm table-centered mb-0 font-14">
+      <thead className="table-light">
+        {headerGroups.map((headerGroup) => (
+          <tr {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map((column) => (
+              <th {...column.getHeaderProps(column.sort && column.getSortByToggleProps())}>
+                <span {...column.getHeaderProps(column.getSortByToggleProps())}>
+                  {column.render('Header')}
+                  {column.isSorted ? (column.isSortedDesc ? '🔽' : '🔼') : ''}
+                </span>
+              </th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+      <tbody {...getTableBodyProps()}>
+        {rows.map((row) => {
+          prepareRow(row);
+          return (
+            <tr {...row.getRowProps()}>
+              {row.cells.map((cell) => (
+                <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+              ))}
+            </tr>
+          );
+        })}
+      </tbody>
+    </Table>
+  );
+};
+
+export default SortableTable;
