b1021c1b4f478bba6696efc821660a72f173d186
hunhunseunghun <hunhunseunghun@gmail.com>
2023-10-12 17:31:35 +0900

    blob, base64 인코딩 문자열 변환

--- src/components/FileUploader.js
+++ src/components/FileUploader.js
@@ -44,16 +44,17 @@ const FileUploader = (props: FileUploaderProps): React$Element<any> => {
             allFiles.push(...files);
             setSelectedFiles(allFiles);
 
-            const encodedBase64 = [...selectedFiles].map((file) => {
-                const reader = new FileReader();
-                reader.onload = () => {
-                    const base64Data = reader.result;
-                    console.log(base64Data);
-                };
-                reader.readAsDataURL(file);
-            });
+            // blob 데이터 base64 인코딩 문자열  변환
+            // const encodedBase64 = [...selectedFiles].map((file) => {
+            //     const reader = new FileReader();
+            //     reader.onload = () => {
+            //         const base64Data = reader.result;
+            //         console.log(base64Data);
+            //     };
+            //     reader.readAsDataURL(file);
+            // });
 
-            console.log('encodedBase64 : ', encodedBase64);
+            // console.log('encodedBase64 : ', encodedBase64);
         }
 
         /**
